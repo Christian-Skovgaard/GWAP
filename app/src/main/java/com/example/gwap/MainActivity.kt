ea3e0d88982b3a31e25349320a3b5839166196ee
Christian-Skovgaard <cellochr@gmail.com>
2025-03-06 12:30:41 +0100

    home done

--- app/src/main/java/com/example/gwap/MainActivity.kt
+++ app/src/main/java/com/example/gwap/MainActivity.kt
@@ -4,14 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -37,9 +45,60 @@ fun navComponent () {
     NavHost(navController = navController, startDestination = "homeScreen", builder = {
         composable("homeScreen") {HomeScreen(navController)}
         composable("CreateProfile1") {CreateProfile1()}
+        composable("StartScreen") {StartScreen()}
     })
 }
 
+@Preview(showBackground = true)
+@Composable
+fun StartScreen () {
+    Box (
+        modifier = Modifier
+            //.background(color= Color.Red)
+            .fillMaxSize()
+        ,
+        contentAlignment = Alignment.Center
+    ) {
+        Column (
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxWidth(0.8f)
+        ) {
+            Box () {
+                Text(
+                    text="Gwap",
+                    fontSize = 100.sp
+                )
+            }
+            Box {
+                Column {
+                    Button(
+                        onClick = {},
+                        modifier = Modifier.fillMaxWidth(0.8f)
+                    ) {
+                        Text(text="Opret Profil")
+                    }
+                    Button(
+                        onClick = {},
+                        modifier = Modifier.fillMaxWidth(0.8f)
+                    ) {
+                        Text(text="Log In")
+                    }
+                }
+            }
+        }
+        Row (
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(bottom = 60.dp),
+            verticalAlignment = Alignment.Bottom,
+            horizontalArrangement = Arrangement.SpaceAround
+        ) {
+            Text(text = "Byt.", fontSize = 20.sp)
+            Text(text = "Brug.", fontSize = 20.sp)
+            Text(text = "Bevar.", fontSize = 20.sp)
+        }
+    }
+}
 
 @Composable
 fun HomeScreen (navController: NavController) {
@@ -60,3 +119,4 @@ fun HomeScreen (navController: NavController) {
 fun CreateProfile1 () {
     Text ("yes")
 }
+
