74a16bf0f7dcdd21d23a83b8edcce818fcf26bc4
Christian-Skovgaard <cellochr@gmail.com>
2025-03-06 16:02:37 +0100

    textbox sortoff done

--- app/src/main/java/com/example/gwap/Components.kt
+++ app/src/main/java/com/example/gwap/Components.kt
@@ -1,8 +1,14 @@
 package com.example.gwap
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -16,19 +22,28 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Preview(showBackground = true)
 @Composable
 fun InputField () {
-    Column() {
-        Text(text="text")
+    Column(
+        modifier = Modifier.padding(10.dp)
+    ) {
+        Text(
+            text="text",
+            modifier = Modifier.padding(bottom = 5.dp)
+        )
         var text by remember {mutableStateOf(TextFieldValue(""))}
-        TextField(
+        BasicTextField (
             value = text,
             onValueChange = {newText -> text = newText},
-            modifier = Modifier.background(color = Color.White),
-            colors = TextFieldDefaults.outlinedTextFieldColors(containerColor = Color.White)
+            modifier = Modifier
+                .border(width = 2.dp, color = Color(android.graphics.Color.parseColor("#D0D0D0")), RoundedCornerShape(3.dp))
+                .fillMaxWidth()
         )
     }
 }
+
